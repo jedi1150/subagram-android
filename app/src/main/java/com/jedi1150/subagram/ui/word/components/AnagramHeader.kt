f7ac32f7deccc30cdb5f5bd6b05abf69192fa258
Oleg Yakovlev <oyakovlev@voximplant.com>
2022-11-09 17:30:56 +0300

    WordScreen anagrams counter animation added

--- app/src/main/java/com/jedi1150/subagram/ui/word/components/AnagramHeader.kt
+++ app/src/main/java/com/jedi1150/subagram/ui/word/components/AnagramHeader.kt
@@ -1,8 +1,7 @@
 package com.jedi1150.subagram.ui.word.components
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
+import androidx.compose.animation.*
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardColors
 import androidx.compose.material3.CardDefaults
@@ -16,6 +15,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jedi1150.subagram.R
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun AnagramHeader(
     size: Int,
@@ -32,10 +32,24 @@ fun AnagramHeader(
             shape = shape,
             colors = colors,
         ) {
-            Text(
-                text = stringResource(id = R.string.anagrams_count, size),
-                modifier = Modifier.padding(paddingValues),
-            )
+            Row(modifier = Modifier.padding(paddingValues),
+                horizontalArrangement = Arrangement.spacedBy(4.dp)) {
+                Text(
+                    text = stringResource(id = R.string.anagrams),
+                )
+                AnimatedContent(
+                    targetState = size,
+                    transitionSpec = {
+                        if (targetState > initialState) {
+                            slideInVertically { height -> height } + fadeIn() with slideOutVertically { height -> -height } + fadeOut()
+                        } else {
+                            slideInVertically { height -> -height } + fadeIn() with slideOutVertically { height -> height } + fadeOut()
+                        }.using(SizeTransform(clip = false))
+                    },
+                ) { targetCount ->
+                    Text(text = targetCount.toString())
+                }
+            }
         }
     }
 }
